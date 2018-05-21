c95fb720e44aebe5df39d928936946991e762a4b
Codename: Steeve Knight <steeve@dharmiweb.net>
2018-05-21 11:27:49 -0400

    implementing DetailsPlugin in prep for sybling app;

---
+++ src/DetailsPluginApp.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <header>
+        </header>
+        <aside>
+            <div class="row">
+                <div class="col s12">
+                    <app-notifications/>
+                </div>
+            </div>
+        </aside>
+        <main>
+            <recipe-details-plugin/>
+        </main>
+    </div>
+</template>
+
+<script>
+import Notifications from '@/components/notifications/Notifications';
+import RecipeDetailsPlugin from '@/components/recipe-details/RecipeDetailsPlugin';
+export default {
+  name: 'details-plugin',
+  components: {
+    'app-notifications': Notifications,
+    'recipe-details-plugin': RecipeDetailsPlugin
+  }
+};
+</script>
+
+<style src="@/assets/app.css"/>
