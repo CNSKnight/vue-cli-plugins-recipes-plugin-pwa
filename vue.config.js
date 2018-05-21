8f7e8851ba6e487ca11370c5969b7f9106073a5c
Codename: Steeve Knight <steeve@dharmiweb.net>
2018-05-21 17:41:43 -0400

    add baseUrl for prod build;

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 module.exports = {
+    baseUrl: '/RecipeDetails/',
     configureWebpack: {
         entry: {
-        detailsPlugin: ['./src/details-plugin.js'],
+            detailsPlugin: ['./src/details-plugin.js'],
         },
         plugins: [
             new HtmlWebpackPlugin({
                 template: './public/index-plugin.html',
                 title: 'Webpack Recipe Details Plugin App',
                 inject: 'body',
-                filename: 'index-plugin.html'
+                filename: 'index-plugin.html',
+                excludeChunks: ['app']
             }),
             new webpack.optimize.CommonsChunkPlugin({
                 name: 'common'
