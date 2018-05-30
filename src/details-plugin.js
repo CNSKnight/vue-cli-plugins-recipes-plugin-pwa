7325a74e5e7ff0eac81cc785e850f1dff4a5060b
Codename: Steeve Knight <steeve@dharmiweb.net>
2018-05-30 08:27:46 -0400

    point to separage store; mount token chng;

--- src/details-plugin.js
+++ src/details-plugin.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import store from './store';
+import store from './store/index-plugin';
 import '@/components/_globals';
 import DetailsPluginApp from './DetailsPluginApp';
 
@@ -11,4 +11,4 @@ Vue.config.productionTip = false;
 new Vue({
   store,
   render: h => h(DetailsPluginApp)
-}).$mount('#plugin');
+}).$mount('#detailsPlugin');
