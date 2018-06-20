d3781c3d1565193bad49671dae098e5caa7c818e
Codename: Steeve Knight <steeve@dharmiweb.net>
2018-06-20 13:20:43 -0400

    unneeded entry;

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const path = require('path');
 module.exports = {
-    baseUrl: process.env.NODE_ENV == 'production' ? '/RecipeDetails/' : '/',
-    configureWebpack: {
-        entry: {
-            detailsPlugin: ['./src/details-plugin.js'],
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './public/index-plugin.html',
-                title: 'Webpack Recipe Details Plugin App',
-                inject: 'body',
-                filename: 'index-plugin.html',
-                excludeChunks: ['app']
-            })
-        ],
-        output: {
-            chunkFilename: '[name].bundle.js',
-        },
-        resolve: {
-            alias: {
-            }
-        }
+  baseUrl: process.env.NODE_ENV == 'production' ? '/RecipeDetails/' : '/',
+  configureWebpack: {
+    entry: {
+      detailsPlugin: ['./src/details-plugin.js']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index-plugin.html',
+        title: 'Webpack Recipe Details Plugin App',
+        inject: 'body',
+        filename: 'index-plugin.html',
+        excludeChunks: ['app']
+      })
+    ],
+    output: {
+      chunkFilename: '[name].bundle.js'
+    },
+    resolve: {
+      alias: {}
     }
-}
+  }
+};
