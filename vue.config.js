2dc068cbd9eaae3b20eaf85fe391b844e5d2ef38
Codename: Steeve Knight <steeve@dharmiweb.net>
2019-12-06 09:59:18 -0500

    improving build w/pages, splitChunks - testing after npm ugrades;

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,91 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const {BundleStatsWebpackPlugin} = require('bundle-stats');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+/**
+ * DISCLAIMER: Using this plugin without enabling the proper feature sets may cause lodash functions to behave in unexpected ways.
+ */
+const lmrpOpts = {
+  currying: true,
+  collections: true,
+  cloning: true,
+  caching: true,
+  paths: true,
+  metadata: true,
+  placeholders: true,
+};
+
 module.exports = {
   // publicPath: process.env.NODE_ENV == 'production' ? '/RecipeDetails/' : '/',
   publicPath: '/RecipeDetails/',
   outputDir: '/var/www/TAPPADS/vegrds-7109/public_html/acap-dev/plugins/RecipeDetails/dist1/',
-  configureWebpack: {
-    entry: {
-      detailsPluginAppLib: ['./src/details-plugin.js']
+  indexPath: 'templates/',
+  pages: {
+    app: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'App Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['index', 'chunk-big-vendor'],
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index-plugin.html',
-        title: 'Webpack Recipe Details Plugin App',
-        inject: 'body',
-        filename: 'index-plugin.html',
-        excludeChunks: ['app']
-      })
-    ],
-    output: {
-      chunkFilename: '[name].bundle.js'
-    }
+    detailsPlugin: {
+      // entry for the page
+      entry: 'src/details-plugin.js',
+      // the source template
+      template: 'public/index-plugin.html',
+      // output as dist/index.html
+      filename: 'index-plugin.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Details Plugin Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['index-plugin', 'chunk-big-vendor'],
+    },
   },
-  productionSourceMap: true
+
+  configureWebpack: {
+    plugins: [
+      new LodashModuleReplacementPlugin(lmrpOpts),
+      // use `$ BUNDLE_STATS_BASELINE=true npm run build` to establish baseline
+      new BundleStatsWebpackPlugin(),
+      // new BundleAnalyzerPlugin(),
+    ],
+    // Option #1 - for Simple configs
+    // optimization: {
+    //   splitChunks: {
+    //     cacheGroups: {
+    //       bigVendors: {
+    //         name: "chunk-big-vendors",
+    //         test: /\/node_modules\/(lodash|markdown-it)\//,
+    //         chunks: "all",
+    //         priority: 1
+    //       },
+    //       common: {
+    //         priority: -9
+    //       }
+    //     }
+    //   }
+    // }
+  },
+  // Option #2 - for Advanced config
+  chainWebpack: config => {
+    // config.optimization.get("splitChunks").cacheGroups.bigVendors = {
+    //   name: "chunk-big-vendors",
+    //   test: /\/node_modules\/(lodash|markdown-it)\//,
+    //   chunks: "all",
+    //   priority: 1
+    // };
+    // config.optimization.get("splitChunks").cacheGroups.common.priority = -9;
+    console.log(config.optimization.get('splitChunks'));
+  },
+  productionSourceMap: true,
 };
