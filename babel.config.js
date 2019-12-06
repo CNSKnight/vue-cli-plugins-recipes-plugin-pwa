d59185f53f2cbc70838acc6a063f3e3f4f3ce4dc
Codename: Steeve Knight <steeve@dharmiweb.net>
2019-12-06 10:30:19 -0500

    vue-cli upgrade;

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
 
-  const presets = ['@vue/app', ['@babel/preset-env', {modules: false, targets: {node: 6}}]];
+  const presets = ['@vue/cli-plugin-babel/preset', ['@babel/preset-env', {modules: false, targets: {node: 6}}]];
   const plugins = [
     'lodash',
     // '@babel/plugin-proposal-optional-chaining'
