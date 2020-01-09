5224eea4ceb1eb59d47dcef0f047d3cb2a525d2d
Codename: Steeve Knight <steeve@dharmiweb.net>
2020-01-08 19:31:45 -0500

    formatting

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = function(api) {
   api.cache(true);
 
-  const presets = ['@vue/cli-plugin-babel/preset', ['@babel/preset-env', {modules: false, targets: {node: 6}}]];
-  const plugins = [
-    'lodash',
-    // '@babel/plugin-proposal-optional-chaining'
+  const presets = [
+    '@vue/cli-plugin-babel/preset',
+    ['@babel/preset-env', { modules: false, targets: { node: 6 } }]
   ];
+  const plugins = ['lodash', '@babel/plugin-proposal-optional-chaining'];
 
   return {
     presets,
-    plugins,
+    plugins
   };
 };
