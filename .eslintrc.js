eee6b3cf0ea8d5e65fb9dea4c3d2dc963cdae748
Codename: Steeve Knight <steeve@dharmiweb.net>
2020-02-01 10:42:52 -0500

    _/fp config;

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,14 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/recommended', '@vue/prettier' /*, 'plugin:lodash-fp/recommended'*/],
+  extends: ['eslint:recommended', 'plugin:vue/recommended', '@vue/prettier', 'plugin:lodash-fp/recommended'],
   plugins: ['lodash', 'lodash-fp', 'vue'],
   parserOptions: {parser: 'babel-eslint', ecmaVersion: 2017, sourceType: 'module'},
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-unused-vars': 'warn',
+    'lodash-fp/use-fp': 'warn',
+    'lodash-fp/no-unused-result': 'warn',
   },
 };
