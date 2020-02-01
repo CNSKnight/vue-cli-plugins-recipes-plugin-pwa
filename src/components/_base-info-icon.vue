f10e089af0939dbf828064a0c2870c28742b28a2
Codename: Steeve Knight <steeve@dharmiweb.net>
2020-01-31 20:57:36 -0500

    updated _base-globals loader; added info-icon;

---
+++ src/components/_base-info-icon.vue
@@ -0,0 +1,15 @@
+<template>
+  <i :class="`material-icons left ${severity} acap_${severity}`" :title="title">
+    info
+  </i>
+</template>
+
+<script>
+export default {
+  name: 'InfoIcon',
+  props: {
+    severity: { type: String, default: 'info' },
+    title: { type: String, default: '' }
+  }
+};
+</script>
