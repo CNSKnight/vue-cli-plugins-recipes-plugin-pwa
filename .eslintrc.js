051442318802506a16446fb9e176774519f92efc
Codename: Steeve Knight <steeve@dharmiweb.net>
2021-07-19 17:34:32 -0400

    idk...ran `$ vue create re3cipes-plugin-pwa` w/merge

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,25 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
+    node: true
   },
-  extends: ['eslint:recommended', 'plugin:vue/recommended', '@vue/prettier', 'plugin:lodash-fp/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    'plugin:lodash-fp/recommended'
+  ],
   plugins: ['lodash', 'lodash-fp', 'vue'],
-  parserOptions: {parser: 'babel-eslint', ecmaVersion: 2017, sourceType: 'module'},
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-unused-vars': 'warn',
     'lodash-fp/use-fp': 'warn',
-    'lodash-fp/no-unused-result': 'warn',
-  },
+    'lodash-fp/no-unused-result': 'warn'
+  }
 };
--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
-module.exports = function(api) {
-  api.cache(true);
-
-  const presets = [
-    '@vue/cli-plugin-babel/preset',
-    ['@babel/preset-env', { modules: false, targets: { node: 6 } }]
-  ];
-  const plugins = ['lodash', '@babel/plugin-proposal-optional-chaining'];
-
-  return {
-    presets,
-    plugins
-  };
+module.exports = {
+  presets: ['@vue/cli-plugin-babel/preset']
 };
