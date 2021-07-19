1899571795443ab38237bb7772d49d1bba775b06
Codename: Steeve Knight <steeve@dharmiweb.net>
2021-07-19 18:35:27 -0400

    re-vue add vuex router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
-import About from '@/views/About.vue';
 // import Hello from '@/components/Hello'
 import Recipes from '@/components/recipes/Recipes';
 import RecipeDetailsPlugin from '@/components/recipe-details/RecipeDetailsPlugin.vue';
 
 Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    },
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
     {
       path: '/recipes',
       name: 'Recipes',
@@ -32,3 +34,9 @@ export default new Router({
     }
   ]
 });
+
+const router = new Router({
+  routes
+});
+
+export default router;
